81bea9205642d7428da096bbdc4e48a7c72e3de3
Bruno Reolon <bruno.reolonn@gmail.com>
2019-10-27 02:55:11 -0300

    adicionado metodos remove, update, search

--- src/main/java/br/com/unipar/gdsystem/dao/ClienteDAO.java
+++ src/main/java/br/com/unipar/gdsystem/dao/ClienteDAO.java
@@ -1,6 +1,14 @@
 package br.com.unipar.gdsystem.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import br.com.unipar.gdsystem.model.Cliente;
 import br.com.unipar.gdsystem.util.JPAUtil;
@@ -20,4 +28,72 @@ public class ClienteDAO {
 
 		manager.close();
 	}
+	
+	public List<Cliente> getList(){
+		CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+		CriteriaQuery<Cliente> query = criteriaBuilder.createQuery(Cliente.class);
+		Root<Cliente> root = query.from(Cliente.class);
+		
+		query.select(root);
+		
+		TypedQuery<Cliente> typedQuery = manager.createQuery(query);
+		
+		return typedQuery.getResultList();
+	}
+	
+	public void remove(Integer id) {
+		Cliente cliente = search(id);
+		
+		manager.getTransaction().begin();
+		manager.remove(cliente);
+		manager.getTransaction().commit();
+	}
+	
+	public void update() {
+		getList();
+	}
+	
+	public Cliente search(Integer id) {
+		CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+		CriteriaQuery<Cliente> query = criteriaBuilder.createQuery(Cliente.class);
+		Root<Cliente> root = query.from(Cliente.class);
+		
+		Path<String> idPath = root.<String>get("id");
+		
+		Predicate nomeIgual = criteriaBuilder.equal(idPath, id);
+		
+		query.where(nomeIgual);
+		
+		TypedQuery<Cliente> typedQuery = manager.createQuery(query);
+		
+		return typedQuery.getSingleResult();
+	}
+	
+//	public List<Cliente> searchNome(String nome) {
+//		Session session = manager.unwrap(Session.class);
+//		Criteria criteria = session.createCriteria(Cliente.class);
+//		
+//		criteria.add(Restrictions.ilike("nome", "%" + nome + "%", MatchMode.ANYWHERE));
+//		
+//		return (List<Cliente>) criteria.list();
+//	}
+//	
+//	public Cliente searchCpf(String cpf) {
+//		Session session = manager.unwrap(Session.class);
+//		Criteria criteria = session.createCriteria(Cliente.class);
+//		
+//		criteria.add(Restrictions.ilike("cpf", "%" + cpf + "%", MatchMode.ANYWHERE));
+//		
+//		return (Cliente) criteria.setFirstResult(1);
+//	}
+//	
+//	public Cliente searchRg(String rg) {
+//		Session session = manager.unwrap(Session.class);
+//		Criteria criteria = session.createCriteria(Cliente.class);
+//		
+//		criteria.add(Restrictions.ilike("rg", "%" + rg + "%", MatchMode.ANYWHERE));
+//		
+//		return (Cliente) criteria.list();
+//	}
+	
 }
--- src/main/java/br/com/unipar/gdsystem/dao/ProdutoDAO.java
+++ src/main/java/br/com/unipar/gdsystem/dao/ProdutoDAO.java
@@ -1,6 +1,14 @@
 package br.com.unipar.gdsystem.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import br.com.unipar.gdsystem.model.Produto;
 import br.com.unipar.gdsystem.util.JPAUtil;
@@ -11,12 +19,52 @@ public class ProdutoDAO {
 	public ProdutoDAO() {
 		manager = new JPAUtil().getEntityManager();
 	}
-	
+
 	public void add(Produto produto) {
 		manager.getTransaction().begin();
 		manager.persist(produto);
 		manager.getTransaction().commit();
 
 		manager.close();
+	}
+
+	public List<Produto> getList() {
+		CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+		CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
+		Root<Produto> root = query.from(Produto.class);
+
+		query.select(root);
+
+		TypedQuery<Produto> typedQuery = manager.createQuery(query);
+
+		return typedQuery.getResultList();
+	}
+	
+	public void remove(Integer id) {
+		Produto produto = search(id);
+		
+		manager.getTransaction().begin();
+		manager.remove(produto);
+		manager.getTransaction().commit();
+	}
+	
+	public void update() {
+		getList();
+	}
+	
+	public Produto search(Integer id) {
+		CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+		CriteriaQuery<Produto> query = criteriaBuilder.createQuery(Produto.class);
+		Root<Produto> root = query.from(Produto.class);
+		
+		Path<String> idPath = root.<String>get("id");
+		
+		Predicate idIgual = criteriaBuilder.equal(idPath, id);
+		
+		query.where(idIgual);
+		
+		TypedQuery<Produto> typedQuery = manager.createQuery(query);
+		
+		return typedQuery.getSingleResult();
 	}
 }
