bcdb2e51e43bb304af875d7b8c8e845951d48358
Bruno Reolon <bruno.reolonn@gmail.com>
2019-10-27 02:54:02 -0300

    adicionados metodos atualizar, remover getId

--- src/main/java/br/com/unipar/gdsystem/controller/VisualizarClienteController.java
+++ src/main/java/br/com/unipar/gdsystem/controller/VisualizarClienteController.java
@@ -1,62 +1,132 @@
 package br.com.unipar.gdsystem.controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import br.com.unipar.gdsystem.dao.ClienteDAO;
+import br.com.unipar.gdsystem.model.Cliente;
 import br.com.unipar.gdsystem.util.OpenCloseStage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class VisualizarClienteController {
+public class VisualizarClienteController implements Initializable {
 
 	private static Stage stage;
+	private ClienteDAO clienteDao;
+//	private Cliente cliente;
 	
-	@FXML
-    private AnchorPane apVisualizarCliente;
-
-    @FXML
-    private HBox hbFiltro;
-
-    @FXML
-    private TableView<?> tvCliente;
-
-    @FXML
-    private TableColumn<?, ?> tbcId;
-
-    @FXML
-    private TableColumn<?, ?> tbcNome;
-
-    @FXML
-    private TableColumn<?, ?> tbcCpf;
-
-    @FXML
-    private TableColumn<?, ?> tbcTelefone;
-
-    @FXML
-    private TableColumn<?, ?> tbcCelular;
-
-    @FXML
-    private TableColumn<?, ?> tbcEndereco;
-
-    @FXML
-    private Button btnAdicionar;
-
-    @FXML
-    void onAbrirCadClienteAction(ActionEvent event) throws IOException {
-    	OpenCloseStage.loadStage("/br/com/unipar/gdsystem/view/CadastroCliente.fxml", "Cadastro de Cliente", false);
-    	setStage(OpenCloseStage.getStage());
-    }
+	@FXML private AnchorPane apVisualizarCliente;
+	@FXML private HBox hbFiltro;
+	@FXML private TextField txtNome;
+    @FXML private Button btnNome;
+    @FXML private TextField txtCpf;
+    @FXML private Button btnCpf;
+    @FXML private TextField txtRg;
+    @FXML private Button btnRg;
+	@FXML private TableView<Cliente> tvCliente;
+	@FXML private TableColumn<Cliente, Integer> tbcId;
+	@FXML private TableColumn<Cliente, String> tbcNome;
+	@FXML private TableColumn<Cliente, String> tbcCpf;
+	@FXML private TableColumn<Cliente, String> tbcTelefone;
+	@FXML private TableColumn<Cliente, String> tbcCelular;
+	@FXML private TableColumn<Cliente, String> tbcEndereco;
+	@FXML private Button btnAdicionar;
+	@FXML private Button btnAtualizar;
+	@FXML private Button btnRemover;
 
 	public void setStage(Stage stage) {
 		VisualizarClienteController.stage = stage;
 	}
-	
+
 	public static Stage getStage() {
 		return stage;
+	}
+
+	@FXML
+	void onPesquisarNome(ActionEvent event) {
+//		clienteDao.searchNome(txtNome.getText());
+		
+	}
+
+	@FXML
+	void onPesquisarCpf(ActionEvent event) {
+//		cliente = clienteDao.searchCpf(txtCpf.getText());
+	}
+
+	@FXML
+	void onPesquisarRg(ActionEvent event) {
+//		cliente = clienteDao.searchRg(txtRg.getText());
+	}
+
+	@FXML
+	private void onAbrirCadClienteAction(ActionEvent event) throws IOException {
+		OpenCloseStage.loadStage("/br/com/unipar/gdsystem/view/CadastroCliente.fxml", "Cadastro de Cliente", false);
+		setStage(OpenCloseStage.getStage());
+	}
+
+	@FXML
+	private void onAtualizarAction() {
+		initTable();
+	}
+
+	@FXML
+	private void onRemoverAction() {
+		Integer id = onGetId();
+
+		if (id == -1) {
+			return;
+		}
+
+		clienteDao.remove(id);
+		onAtualizarAction();
+	}
+
+	@FXML
+	public Integer onGetId() {
+		Object object = tvCliente.getSelectionModel().getSelectedItem();
+
+		if (object == null) {
+			return -1;
+		}
+
+		btnRemover.setDisable(false);
+
+		Integer id = ((Cliente) object).getId();
+		
+		return id != null ? id : -1;
+	}
+
+	private void initTable() {
+		tbcId.setCellValueFactory(new PropertyValueFactory<>("id"));
+		tbcNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+		tbcCpf.setCellValueFactory(new PropertyValueFactory<>("cpf"));
+		tbcTelefone.setCellValueFactory(new PropertyValueFactory<>("telefone"));
+		tbcCelular.setCellValueFactory(new PropertyValueFactory<>("celular"));
+		tbcEndereco.setCellValueFactory(new PropertyValueFactory<>("endereco"));
+
+		tvCliente.setItems(foo());
+	}
+
+	private ObservableList<Cliente> foo() {
+		clienteDao = new ClienteDAO();
+
+		return FXCollections.observableArrayList(clienteDao.getList());
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		initTable();
 	}
 }
--- src/main/java/br/com/unipar/gdsystem/controller/VisualizarProdutoController.java
+++ src/main/java/br/com/unipar/gdsystem/controller/VisualizarProdutoController.java
@@ -1,10 +1,15 @@
 package br.com.unipar.gdsystem.controller;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import br.com.unipar.gdsystem.dao.ProdutoDAO;
+import br.com.unipar.gdsystem.model.Produto;
 import br.com.unipar.gdsystem.util.OpenCloseStage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -13,66 +18,35 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class VisualizarProdutoController implements Initializable {
 
 	private static Stage stage;
+	private ProdutoDAO produtoDao = new ProdutoDAO();
 	
-	@FXML
-	private AnchorPane apCadProduto;
-
-	@FXML
-	private Label lblCodigo;
-
-	@FXML
-	private Label lblDepartamento;
-
-	@FXML
-	private Label lblMarca;
-
-	@FXML
-	private TextField txCodigo;
-
-	@FXML
-	private TextField txtDepartamento;
-
-	@FXML
-	private TextField txtMarca;
-
-	@FXML
-	private Button btnCodigo;
-
-	@FXML
-	private Button btnDepartamento;
-
-	@FXML
-	private Button btnMarca;
-
-	@FXML
-	private TableView<?> tvProduto;
-
-	@FXML
-	private TableColumn<?, ?> tbcCodigo;
-
-	@FXML
-	private TableColumn<?, ?> tbcDescricao;
-
-	@FXML
-	private TableColumn<?, ?> tbcUn;
-
-	@FXML
-	private TableColumn<?, ?> tbcDepartamento;
-
-	@FXML
-	private TableColumn<?, ?> tbcEstoque;
-
-	@FXML
-	private TableColumn<?, ?> tbcValorVenda;
-
-	@FXML
-	private Button btnCadastrar;
+	@FXML private AnchorPane apCadProduto;
+	@FXML private Label lblCodigo;
+	@FXML private Label lblDepartamento;
+	@FXML private Label lblMarca;
+	@FXML private TextField txtCodigo;
+	@FXML private TextField txtDepartamento;
+	@FXML private TextField txtMarca;
+	@FXML private Button btnCodigo;
+	@FXML private Button btnDepartamento;
+	@FXML private Button btnMarca;
+	@FXML private TableView<Produto> tvProduto;
+	@FXML private TableColumn<Produto, String> tbcCodigo;
+	@FXML private TableColumn<Produto, String> tbcDescricao;
+	@FXML private TableColumn<Produto, String> tbcUn;
+	@FXML private TableColumn<Produto, String> tbcDepartamento;
+	@FXML private TableColumn<Produto, Integer> tbcEstoque;
+	@FXML private TableColumn<Produto, BigDecimal> tbcValorVenda;
+	@FXML private Button btnCadastrar;
+	@FXML private Button btnAtualizar;
+	@FXML private Button btnRemover;
 
 	@FXML
 	void onCadProdutoAction(ActionEvent event) throws IOException {
@@ -87,10 +61,55 @@ public class VisualizarProdutoController implements Initializable {
 	public static Stage getStage() {
 		return stage;
 	}
+	
+	@FXML
+	private void onAtualizarAction() {
+		initTable();
+	}
+
+	@FXML
+	private void onRemoverAction() {
+		Integer id = onGetId();
+
+		if (id == -1) {
+			return;
+		}
+		produtoDao.remove(id);
+		onAtualizarAction();
+	}
+
+	@FXML
+	public Integer onGetId() {
+		Object object = tvProduto.getSelectionModel().getSelectedItem();
+
+		if (object == null) {
+			return -1;
+		}
+
+		btnRemover.setDisable(false);
+
+		Integer id = ((Produto) object).getId();
+		
+		return id != null ? id : -1;
+	} 
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		// TODO Auto-generated method stub
-		
+		initTable();
+	}
+	
+	private void initTable() {
+		tbcCodigo.setCellValueFactory(new PropertyValueFactory<>("codigoInterno"));
+		tbcDescricao.setCellValueFactory(new PropertyValueFactory<>("descricao"));
+		tbcUn.setCellValueFactory(new PropertyValueFactory<>("unidade"));
+		tbcDepartamento.setCellValueFactory(new PropertyValueFactory<>("departamento"));
+		tbcEstoque.setCellValueFactory(new PropertyValueFactory<>("quantidadeTotal"));
+		tbcValorVenda.setCellValueFactory(new PropertyValueFactory<>("valorVenda"));
+
+		tvProduto.setItems(foo());
+	}
+	
+	private ObservableList<Produto> foo(){
+		return FXCollections.observableArrayList(produtoDao.getList());
 	}
 }
